2231cfabf3765275e523c7ba4285da8dc709abeb
Szymon Kozioł <szymon.koziol@deployed.pl>
2025-05-29 13:08:22 +0200

    Prettier configuration

--- prettier.config.js
+++ prettier.config.js
@@ -9,9 +9,11 @@ const config = {
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ['prettier-plugin-tailwindcss', '@trivago/prettier-plugin-sort-imports'],
   tailwindStylesheet: './src/index.css',
   tailwindFunctions: ['clsx', 'cva'],
+  importOrder: ['^react$', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^../(.*)$', '^./(.*)$'],
+  importOrderSeparation: true,
 };
 
 export default config;
